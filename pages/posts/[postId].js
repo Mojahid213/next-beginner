9a92a039fa5de80560502cc323f377551a3299f8
Mojahid213 <mojoffi133@gmail.com>
2021-11-10 19:30:05 +0600

    SSG with getStaticPaths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -4,9 +4,26 @@ export default function Post({ post }) {
       <h1 className="text-3xl">
         {post.id} - {post.title}
       </h1>
-      <p className="text-2xl">{post.body}</p>
+      <p className="text-2xl mt-5">{post.body}</p>
     </>
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: { postId: "1" },
+      },
+      {
+        params: { postId: "2" },
+      },
+      {
+        params: { postId: "3" },
+      },
+    ],
+    fallback: false,
+  };
 }
 
 export async function getStaticProps(context) {
