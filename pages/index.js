c6e23b3fc769f0156fc5bd3c92800f7faf08d524
Mojahid213 <mojoffi133@gmail.com>
2021-11-10 10:33:30 +0600

    Navigating Programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("Placing your order");
+    router.push("/product");
+  };
   return (
     <div>
       <h1 className="text-4xl text-center">Home page</h1>
@@ -11,6 +17,12 @@ export default function Home() {
       <Link href="/product">
         <a>Product Page</a>
       </Link>
+      <button
+        onClick={handleClick}
+        className="p-3 bg-green-500 block mt-5 rounded-lg ml-5 focus:ring-2 focus:ring-black ring-offset-2 transition duration-200"
+      >
+        Place order
+      </button>
     </div>
   );
 }
