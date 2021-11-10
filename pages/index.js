6dc969478ad7b6808e116fe62d9a273be249f2cb
Mojahid213 <mojoffi133@gmail.com>
2021-11-10 10:21:22 +0600

    Link Component Navigation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-4xl text-center">Home page</h1>
+      <Link href="/blog">
+        <a>Blog Page</a>
+      </Link>
+      <br />
+      <Link href="/product">
+        <a>Product Page</a>
+      </Link>
     </div>
   );
 }
